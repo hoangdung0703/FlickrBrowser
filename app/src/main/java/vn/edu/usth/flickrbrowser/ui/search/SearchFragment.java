501b458a68e03935034fcbbe4f242ac3b15696f8
ngmnduc <minhductn0408@gmail.com>
2025-09-29 15:32:40 +0700

    feat(ui): add EmpEmptyView + ShimmerSkeleton + PhotoState

--- app/src/main/java/vn/edu/usth/flickrbrowser/ui/search/SearchFragment.java
+++ app/src/main/java/vn/edu/usth/flickrbrowser/ui/search/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,12 +16,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import vn.edu.usth.flickrbrowser.R;
+import vn.edu.usth.flickrbrowser.core.model.PhotoItem;
 import vn.edu.usth.flickrbrowser.databinding.FragmentSearchBinding;
 import vn.edu.usth.flickrbrowser.ui.common.GridSpacingDecoration;
+import vn.edu.usth.flickrbrowser.ui.common.PhotosAdapter;
+import vn.edu.usth.flickrbrowser.ui.state.PhotoState;
 
 public class SearchFragment extends Fragment {
     private FragmentSearchBinding binding;
-
+    private PhotosAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -31,6 +36,10 @@ public class SearchFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        adapter = new PhotosAdapter();
+        binding.rvPhotos.setAdapter(adapter);
+
         // 1) AppBar title
         binding.topAppBar.setTitle("search");
 
@@ -61,6 +70,64 @@ public class SearchFragment extends Fragment {
         super.onDestroyView();
         binding = null;  // tránh leak
     }
+
+    private void setState(@NonNull PhotoState state) {
+        if (state instanceof PhotoState.Loading) {
+            binding.shimmerGrid.getRoot().setVisibility(View.VISIBLE);
+            startShimmers(binding.shimmerGrid.getRoot());
+
+            binding.rvPhotos.setVisibility(View.GONE);
+            binding.emptyView.getRoot().setVisibility(View.GONE);
+
+        } else if (state instanceof PhotoState.Success) {
+            List<PhotoItem> items = ((PhotoState.Success) state).getItems();
+            stopShimmers(binding.shimmerGrid.getRoot());
+            binding.shimmerGrid.getRoot().setVisibility(View.GONE);
+
+            binding.emptyView.getRoot().setVisibility(View.GONE);
+            binding.rvPhotos.setVisibility(View.VISIBLE);
+            adapter.submitList(items);
+
+        } else if (state instanceof PhotoState.Empty) {
+            stopShimmers(binding.shimmerGrid.getRoot());
+            binding.shimmerGrid.getRoot().setVisibility(View.GONE);
+
+            binding.rvPhotos.setVisibility(View.GONE);
+            binding.emptyView.getRoot().setVisibility(View.VISIBLE);
+            binding.emptyView.emptyText.setText(R.string.empty_default);
+
+        } else if (state instanceof PhotoState.Error) {
+            stopShimmers(binding.shimmerGrid.getRoot());
+            binding.shimmerGrid.getRoot().setVisibility(View.GONE);
+
+            binding.rvPhotos.setVisibility(View.GONE);
+            binding.emptyView.getRoot().setVisibility(View.GONE);
+
+            String msg = ((PhotoState.Error) state).getMessage();
+            Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startShimmers(View root) {
+        if (root instanceof com.facebook.shimmer.ShimmerFrameLayout) {
+            ((com.facebook.shimmer.ShimmerFrameLayout) root).startShimmer();
+        }
+        if (root instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) root;
+            for (int i = 0; i < vg.getChildCount(); i++) startShimmers(vg.getChildAt(i));
+        }
+    }
+
+    private void stopShimmers(View root) {
+        if (root instanceof com.facebook.shimmer.ShimmerFrameLayout) {
+            ((com.facebook.shimmer.ShimmerFrameLayout) root).stopShimmer();
+        }
+        if (root instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) root;
+            for (int i = 0; i < vg.getChildCount(); i++) stopShimmers(vg.getChildAt(i));
+        }
+    }
+
 }
 
 //Biding sinh từ fragment_search.xml
