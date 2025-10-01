4fe55c17c586602fd191f1f60165d134573fa5e9
Minh Hieu <minhhieu191105@gmail.com>
2025-10-01 14:41:21 +0700

    strings and fragment favorite update

--- app/src/main/java/vn/edu/usth/flickrbrowser/ui/explore/ExploreAdapter.java
+++ app/src/main/java/vn/edu/usth/flickrbrowser/ui/explore/ExploreAdapter.java
@@ -10,5 +10,5 @@ public class ExploreAdapter extends RecyclerView.Adapter<ExploreAdapter.VH>{
     @NonNull @Override public VH onCreateViewHolder(@NonNull ViewGroup parent,int viewType){ View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_photo,parent,false); return new VH(v); }
     @Override public void onBindViewHolder(@NonNull VH h,int pos){ PhotoItem p=data.get(pos); Glide.with(h.img.getContext()).load(p.getThumbUrl()).into(h.img); }
     @Override public int getItemCount(){ return data.size(); }
-    static class VH extends RecyclerView.ViewHolder{ ImageView img; VH(@NonNull View v){ super(v); img=v.findViewById(R.id.img);} }
+    static class VH extends RecyclerView.ViewHolder{ ImageView img; VH(@NonNull View v){ super(v); img=v.findViewById(R.id.imgPhoto);} }
 }
---
+++ app/src/main/java/vn/edu/usth/flickrbrowser/ui/search/PhotosAdapter.java
@@ -0,0 +1,49 @@
+package vn.edu.usth.flickrbrowser.ui.search;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
+import java.util.List;
+import vn.edu.usth.flickrbrowser.R;
+import vn.edu.usth.flickrbrowser.core.model.PhotoItem;
+
+public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.VH> {
+    private final List<PhotoItem> data = new ArrayList<>();
+
+    public void submitList(List<PhotoItem> items) {
+        data.clear();
+        if (items != null) data.addAll(items);
+        notifyDataSetChanged(); // đủ dùng lúc này; sau có thể đổi sang DiffUtil
+    }
+
+    @NonNull @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_photo, parent, false);
+        return new VH(v);
+    }
+
+    @Override public void onBindViewHolder(@NonNull VH h, int pos) {
+        PhotoItem it = data.get(pos);
+        Glide.with(h.img.getContext())
+                .load(it.getThumbUrl())
+                .placeholder(R.drawable.bg_skeleton_rounded) // tạm
+                .centerCrop()
+                .into(h.img);
+    }
+
+    @Override public int getItemCount() { return data.size(); }
+
+    static class VH extends RecyclerView.ViewHolder {
+        ImageView img;
+        VH(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.imgPhoto);
+        }
+    }
+}
