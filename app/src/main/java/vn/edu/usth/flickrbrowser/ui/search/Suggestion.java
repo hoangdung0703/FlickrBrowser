aebae1190107b2c6250e46f4cce7734bad8ce6b2
ngmnduc <minhductn0408@gmail.com>
2025-10-14 22:56:25 +0700

    add suggestion search

---
+++ app/src/main/java/vn/edu/usth/flickrbrowser/ui/search/Suggestion.java
@@ -0,0 +1,11 @@
+package vn.edu.usth.flickrbrowser.ui.search;
+
+// Model để chứa dữ liệu cho một gợi ý (chỉ cần tên)
+public class Suggestion {
+    public String name;
+
+    public Suggestion(String name) {
+        this.name = name;
+    }
+}
+
